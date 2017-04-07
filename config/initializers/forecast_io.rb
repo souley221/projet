83655e7dcd0b5beab6931808de9db9ff1fc57b62
sollysouley <sollysouley@gmail.com>
2017-04-07 17:43:07 -0400

    Add few changes

--- config/initializers/forecast_io.rb
+++ config/initializers/forecast_io.rb
@@ -1,3 +1,3 @@
 ForecastIO.configure do |configuration|
-  configuration.api_key = Rails.application.secrets.api_key_forecast_io
+  configuration.api_key = Rails.application.secrets.api_key_forecast
 end
